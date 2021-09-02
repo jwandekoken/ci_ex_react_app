6bd2d34639cfe934ff5f5d5d683493ee894932f5
jwandekoken <julio.wandekoken@paketa.com.br>
2021-09-02 08:22:37 -0300

    make test pass again

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders text in link (will fail)", () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/this will fail/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
